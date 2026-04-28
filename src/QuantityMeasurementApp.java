53a70c928d065a1afdda685fea2c6da74fd8348b
Malay-Pal1410 <malaypal954@gmail.com>
2026-04-28 23:33:30 +0530

    UC2:This Use Case extends UC1 to accommodate the Equality Check for Inches along with Feet. This use case is in no way trying to compare two entities, Feet and Inches. They are still treated separately. Please ensure like UC1 the test cases ensure complete test coverage to accurately compare and handle various edge cases

--- src/QuantityMeasurementApp.java
+++ src/QuantityMeasurementApp.java
@@ -11,16 +11,37 @@ public class QuantityMeasurementApp {
         public boolean equals(Object obj) {
             if (this == obj) return true;
             if (obj == null || getClass() != obj.getClass()) return false;
-
             Feet other = (Feet) obj;
             return Double.compare(this.value, other.value) == 0;
         }
     }
 
-    public static void main(String[] args) {
-        Feet f1 = new Feet(1.0);
-        Feet f2 = new Feet(1.0);
+    static class Inches {
+        private final double value;
 
-        System.out.println("Are values equal? " + f1.equals(f2));
+        public Inches(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
+            Inches other = (Inches) obj;
+            return Double.compare(this.value, other.value) == 0;
+        }
+    }
+
+    public static boolean compareFeet(double value1, double value2) {
+        return new Feet(value1).equals(new Feet(value2));
+    }
+
+    public static boolean compareInches(double value1, double value2) {
+        return new Inches(value1).equals(new Inches(value2));
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Feet equality: " + compareFeet(1.0, 1.0));
+        System.out.println("Inches equality: " + compareInches(1.0, 1.0));
     }
 }
